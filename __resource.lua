06b3c333fbbe2ca8be532b03b75b3b5d1b2021d8
Søren <s.benzon@gmail.com>
2018-10-09 13:54:20 +0200

    Clean up

--- __resource.lua
+++ __resource.lua
@@ -1,10 +1,6 @@
 resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
 
-description 'ESX Boilerplate'
-
-server_scripts {
-	'server/main.lua'
-}
+description 'NPC Scare'
 
 client_scripts {
 	'client/main.lua'
